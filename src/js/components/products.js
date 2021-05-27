7a7a5dd460c9d27ba3920d0912ef519fae2ff442
Gerasimov Georgiy Evgenyevich <gegerasimov@ozon.ru>
2021-05-27 04:18:32 +0300

    add modal

--- src/js/components/products.js
+++ src/js/components/products.js
@@ -1,6 +1,13 @@
 const catalogGoodsList = document.querySelector('.catalog__goods-list')
-const catalogMore  = document.querySelector('.catalog__more')
-const prodModal  = document.querySelector('[data-graph-target="prod-modal"] .modal-content')
+const catalogMore = document.querySelector('.catalog__more')
+const prodModal = document.querySelector('[data-graph-target="prod-modal"] .modal-content')
+
+const prodModalSlider = prodModal.querySelector('.modal-slider .swiper-wrapper')
+const prodModalPreview = prodModal.querySelector('.modal-slider .modal-preview')
+const prodModalInfo = prodModal.querySelector('.modal-info__wrapper')
+const prodModalDescr = prodModal.querySelector('.modal-prod-descr')
+const prodModalChars = prodModal.querySelector('.prod-characteristics')
+const prodModalVideo = prodModal.querySelector('.prod-modal__video')
 
 let prodQuantity = 5
 let dataLength = null
@@ -8,6 +15,11 @@ let dataLength = null
 const normalPrice = (str) => {
   return String(str).replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
 };
+
+const prodSwiper = new Swiper('.modal-slider__container', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+});
 
 if (catalogGoodsList) {
   const loadProducts = (quantity = 5) => {
@@ -68,6 +80,8 @@ if (catalogGoodsList) {
             const openBtnId = modal.previousActiveElement.dataset.id;
 
             loadModalData(openBtnId)
+
+            prodSwiper.update()
           },
         })
       })
@@ -83,9 +97,73 @@ if (catalogGoodsList) {
       .then((data) => {
         // prodModal.innerHTML = ''
 
+        prodModalInfo.innerHTML = ''
+        prodModalPreview.innerHTML = ''
+        prodModalDescr.innerHTML = ''
+        prodModalChars.innerHTML = ''
+        prodModalSlider.innerHTML = ''
+        prodModalVideo.innerHTML = ''
+
         for (let dataItem of data) {
           if (dataItem.id == id) {
             console.log(dataItem);
+
+            const slides = dataItem.gallery.map(image => {
+              return `
+              <div class="swiper-slide">
+                <img class="modal-slider__image" src="${image}" alt="">
+              </div>
+
+              `
+            })
+
+            const preview = dataItem.gallery.map((image, idx) => {
+              return `
+                <div class="modal-preview__item" data-index="${idx}">
+                  <img class="modal-preview__img" src="${image}" alt="">
+                </div>
+              `
+            })
+
+            const sizes = dataItem.sizes.map((size) => {
+              return `
+                <li class="modal-sizes__item">
+                  <button class="modal-sizes__btn btn-reset">${size}</button>
+                </li>
+              `
+            })
+
+            prodModalSlider.innerHTML = slides.join('')
+            prodModalPreview.innerHTML = preview.join('')
+
+            prodModalInfo.innerHTML = `
+                <h3 class="modal-info__title">${dataItem.title}</h3>
+
+                <div class="modal-info__rate">
+                  <img src="img/star.svg" alt="Рейтинг 5 из 5">
+                  <img src="img/star.svg" alt="">
+                  <img src="img/star.svg" alt="">
+                  <img src="img/star.svg" alt="">
+                  <img src="img/star.svg" alt="">
+                </div>
+
+                <div class="modal-info__sizes">
+                  <span class="modal-info__subtitle">Выберите размер</span>
+
+                  <ul class="modal-info__sizes-list list-reset modal-sizes">
+                    ${sizes.join('')}
+                  </ul>
+                </div>
+
+                <div class="modal-info__price">
+                  <span class="modal-info__current-price">${dataItem.price} ₽</span>
+                  <span class="modal-info__old-price">${dataItem.oldPrice ? dataItem.oldPrice + ' ₽' : ''}</span>
+                </div>
+            `
+
+            // prodModal.innerHTML = `
+
+            // `
           }
         }
       })
@@ -103,4 +181,5 @@ if (catalogGoodsList) {
     }
   })
 }
+
 
